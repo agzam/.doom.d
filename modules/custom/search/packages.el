649b5f6505966bca75c5fe1c2cdb3b8997d688bd
Ag Ibragimov <agzam.ibragimov@gmail.com>
2025-02-09 17:06:15 -0600

    nope, consult-omni is still breaking

--- modules/custom/search/packages.el
+++ modules/custom/search/packages.el
@@ -5,5 +5,5 @@
   (:host github :repo "armindarvish/consult-omni"
    :branch "develop"
    :files (:defaults "sources/*.el"))
-  ;; :pin "2398ddb53d5f1aa965ab889ed356b6e06624aa27"
+  :pin "2398ddb53d5f1aa965ab889ed356b6e06624aa27"
   )
