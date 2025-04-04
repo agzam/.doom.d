ffd1412ff5240eac69ac20895b5757de00722a98
Ag Ibragimov <agzam.ibragimov@gmail.com>
2025-04-03 21:49:55 -0500

    python stuff

--- modules/custom/python/config.el
+++ modules/custom/python/config.el
@@ -1,5 +1,17 @@
 ;;; custom/python/config.el -*- lexical-binding: t; -*-
 
-(add-hook! (python-ts-mode
-            python-mode)
-           #'lsp!)
+(use-package! python
+  :mode ("[./]flake8\\'" . conf-mode)
+  :mode ("/Pipfile\\'" . conf-mode)
+  :config
+  (add-hook! (python-ts-mode python-mode) #'lsp!))
+
+(use-package! lsp-pyright
+  :defer t
+  :init
+  ;; important: this needs to be set before the package loads
+  (setopt lsp-pyright-multi-root nil)
+  :config
+  (setopt lsp-pyright-langserver-command "basedpyright"
+          lsp-pyright-type-checking-mode "standard"
+          lsp-pyright-auto-search-paths nil))
---
+++ modules/custom/python/packages.el
@@ -0,0 +1,4 @@
+;; -*- no-byte-compile: t; -*-
+;;; custom/python/packages.el
+
+(package! lsp-pyright :recipe (:host github :repo "emacs-lsp/lsp-pyright"))
