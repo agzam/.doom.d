35a5b648578a6f34c919114bc39d741b5a459e67
Ag Ibragimov <agzam.ibragimov@gmail.com>
2025-04-09 17:01:20 -0500

    trying to save/restore desktop. tabs included

--- config.el
+++ config.el
@@ -48,7 +48,16 @@
  ;; doom-unicode-font (font-spec :family "Apple Color Emoji" :size 18)
  )
 
-(desktop-save-mode +1)
+(add-hook! 'doom-after-init-hook
+  (defun init-desktop-mode-h ()
+    (setopt desktop-path (list doom-profile-state-dir))
+    (desktop-save-mode +1))
+
+  (defun restore-desktop-and-tabs-h ()
+    (require 'org-roam-mode)
+    (require 'projectile)
+    (tab-bar-mode 1)
+    (desktop-read doom-profile-state-dir)))
 
 (add-hook! 'doom-load-theme-hook
   (defun reset-fixed-pitch-height-h ()
