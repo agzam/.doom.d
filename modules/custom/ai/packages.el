65061c62e0983b16386825e36f2f4337df854e89
Ag Ibragimov <agzam.ibragimov@gmail.com>
2025-08-23 19:22:00 -0500

    can't figure out that fucking gptel sub

--- modules/custom/ai/packages.el
+++ modules/custom/ai/packages.el
@@ -4,9 +4,7 @@
 (package! whisper :recipe (:host github :repo "natrys/whisper.el"))
 
 (package! gptel
-  :recipe (:host github
-           :repo "karthink/gptel"
-           :files ("!test/*")))
+  :recipe (:host github :repo "agzam/gptel" :files ("*.el")))
 
 (package! gptel-quick :recipe (:host github :repo "karthink/gptel-quick"))
 
