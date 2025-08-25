9effc4e1d183c7deb3c168dc3bd39b49f2bf0274
Ag Ibragimov <agzam.ibragimov@gmail.com>
2025-08-25 16:29:26 -0500

    fix colors, fix gptel

--- modules/custom/ai/packages.el
+++ modules/custom/ai/packages.el
@@ -3,8 +3,10 @@
 
 (package! whisper :recipe (:host github :repo "natrys/whisper.el"))
 
-(package! gptel
-  :recipe (:host github :repo "agzam/gptel" :files ("*.el")))
+(package! gptel)
+
+;; (package! gptel
+;;   :recipe (:host github :repo "agzam/gptel" :files ("*.el")))
 
 (package! gptel-quick :recipe (:host github :repo "karthink/gptel-quick"))
 
