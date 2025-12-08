ddb8b3ce94d5395cc84f248b289a810798d3a550
Ag Ibragimov <agzam.ibragimov@gmail.com>
2025-12-08 10:54:53 -0600

    minor update

--- modules/custom/jira/autoload.el
+++ modules/custom/jira/autoload.el
@@ -22,3 +22,13 @@
               (tab-idx (plist-get btab :tabIndex)))
         (browser-activate-tab win-idx tab-idx)
       (shell-command-to-string (format "%s browse %s" j ticket)))))
+
+
+;;;###autoload
+(defun go-jira-find-pull-requests-on-github (&optional jira-ticket)
+  "Search for mentioning of JIRA-TICKET on GitHub.
+If JIRA-TICKET is not provided, uses ticket at point or prompts."
+  (interactive)
+  (let* ((ticket (or (go-jira--ticket-arg-or-ticket-at-point jira-ticket)
+                     (read-string "Gimme the JIRA ticket to search: "))))
+    (github-topics-find-prs ticket)))
