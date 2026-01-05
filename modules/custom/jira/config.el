478c2bfd01dec17740b5cb9ce2155ce5fd8c29e1
Ag Ibragimov <agzam.ibragimov@gmail.com>
2026-01-05 15:09:26 -0600

    go-jira keys

--- modules/custom/jira/config.el
+++ modules/custom/jira/config.el
@@ -29,6 +29,22 @@
 
     (add-to-list 'embark-keymap-alist '(jira-ticket embark-jira-ticket-map))
 
+    (add-hook! 'go-jira-view-mode-hook
+      (defun go-jira-view-mode-h ()
+        (map!
+         :map go-jira-view-mode-map
+         :nv "C" #'go-jira-add-comment
+         (:prefix ("g" . "go")
+                  "r" #'go-jira-view-mode-refresh))))
+
+    (add-hook! 'go-jira-board-view-mode-hook
+      (defun go-jira-board-view-mode-h ()
+        (map!
+         :map go-jira-board-view-mode-map
+         :nv "C" #'go-jira-add-comment
+         (:prefix ("g" . "go")
+                  "r" #'go-jira-board-refresh))))
+
     (map! :map embark-jira-ticket-map
           (:prefix ("b" . "browse")
            :desc "view" "b" #'go-jira-view-ticket
