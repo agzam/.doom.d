74591d3940c082bfd03a5e0ef44053e892b18593
Ag Ibragimov <agzam.ibragimov@gmail.com>
2026-02-01 16:35:23 -0600

    fix: embark export from vertico
    - I forgot to move the function after removing Doom's vertico module

--- modules/custom/completion/autoload/vertico.el
+++ modules/custom/completion/autoload/vertico.el
@@ -78,3 +78,23 @@
     (when (and (> len 0)
                (char-equal (aref pattern (1- len)) ?$))
       `(orderless-regexp . ,(concat (substring pattern 0 -1) "[\x200000-\x300000]*$")))))
+
+
+;;;###autoload
+(defun +vertico/embark-export-write ()
+  "Export the current vertico results to a writable buffer if possible.
+
+Supports exporting consult-grep to wgrep, file to wdeired, and consult-location to occur-edit"
+  (interactive)
+  (require 'embark)
+  (require 'wgrep)
+  (let* ((edit-command
+          (pcase-let ((`(,type . ,candidates)
+                       (run-hook-with-args-until-success 'embark-candidate-collectors)))
+            (pcase type
+              ('consult-grep #'wgrep-change-to-wgrep-mode)
+              ('file #'wdired-change-to-wdired-mode)
+              ('consult-location #'occur-edit-mode)
+              (x (user-error "embark category %S doesn't support writable export" x)))))
+         (embark-after-export-hook `(,@embark-after-export-hook ,edit-command)))
+    (embark-export)))
